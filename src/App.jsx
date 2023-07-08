fe4a794d412a4948099972a747f31f4e4e75c670
mccooll <mccooll@users.noreply.github.com>
2023-07-08 15:04:41 +0000

    Playing around

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css'
 
-export default function App() {
+const App = ({name}) => {
+  const today = new Date()
   return (
-    <main>
-      Dave's React App ⚛️
-    </main>
+    <div>
+      <p>{name}'s React App ⚛️</p>
+      <p>Made today {today.toDateString()}</p>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
   )
 }
+
+export const FancyParagraph = ({text}) => <p style={{fontWeight:'bold'}}>{text}</p>
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import Application from './App'
+import { FancyParagraph } from './App'
+import { name2 } from './utils' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<App />
+		<Application name="Dave" />
+    <Application name="Anthony" />
+    <Application name={name2} />
+    <FancyParagraph text="Lorem Ipsum" />
 	</React.StrictMode>
 )
