f8693d98fb0d4f9b17a0eb20906ffd01c5bc743b
mccooll <github@no3.ca>
2023-08-12 13:40:54 -0400

    add cat modal

--- src/CatList.jsx
+++ src/CatList.jsx
@@ -4,23 +4,44 @@ import ListItem from '@mui/material/ListItem';
 import List from '@mui/material/List';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import PetsIcon from '@mui/icons-material/Pets';
+import Modal from '@mui/material/Modal';
 
 
 const CatList = ({cats}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedCat, setSelectedCat] = useState({});
+
+  const handleOpen = (cat) => {
+    setIsModalOpen(true);
+    setSelectedCat(cat);
+  }
+
+  const handleClose = () => {
+    setIsModalOpen(false);
+  }
+
   return (
-    <List>
-      {
-        cats.map((c) => (
-            <ListItem key={c.name}>
-              <ListItemIcon>
-                <PetsIcon />
-              </ListItemIcon>
-              <ListItemText>{c.name}</ListItemText>
-            </ListItem>
-          )
-                 )
-      }
-    </List>
+    <>
+      <Modal open={isModalOpen} onClose={()=>handleClose()}>
+        <div className="catModal">
+          <h1>{selectedCat.name}</h1>
+          <div><img src={selectedCat.image} /></div>
+          <p>{selectedCat.desc}</p>
+        </div>
+      </Modal>
+      <List>
+        {
+          cats.map((c) => (
+              <ListItem key={c.name} onClick={()=>handleOpen(c)}>
+                <ListItemIcon>
+                  <PetsIcon />
+                </ListItemIcon>
+                <ListItemText>{c.name}</ListItemText>
+              </ListItem>
+          ))
+        }
+      </List>
+    </>
   )
 }
 
